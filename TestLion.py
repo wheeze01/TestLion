47c2ff4549f9465d1916e4ceec96ec1093c2c2a9
freshman0101 <54202163+wheeze01@users.noreply.github.com>
2024-03-24 16:42:29 +0900

    Update TestLion.py
    longCode

--- TestLion.py
+++ TestLion.py
@@ -1,3 +1,15 @@
-print('hello, world!')
-print('what the fox say')
-print('this is sub main branch')
+def main() :
+    print('hello, world!')
+    print('what the fox say')
+    print('this is sub main branch')
+
+    likeList = [[1,2,3,4,5],[0,6,7,8,9]]
+
+    for i in likeList :
+        for j in i :
+            print(j+", ")
+
+    print('finish')
+
+if __name__ == '__main__' :
+    main()
